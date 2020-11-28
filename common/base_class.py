Add correct action masking for PPO

common/base_class.py
@@ -842,11 +842,17 @@ class ActorCriticRLModel(BaseRLModel):
             state = self.initial_state
         if mask is None:
             mask = [False for _ in range(self.n_envs)]
+
+        action_masks = []
+        if action_mask is not None:
+            for env_action_mask in action_mask:
+                action_masks.append(flatten_action_mask(self.action_space, env_action_mask))
+
         observation = np.array(observation)
         vectorized_env = self._is_vectorized_observation(observation, self.observation_space)
 
         observation = observation.reshape((-1,) + self.observation_space.shape)
-        actions_proba = self.proba_step(observation, state, mask, action_mask)
+        actions_proba = self.proba_step(observation, state, mask, action_masks)
 
         if len(actions_proba) == 0:  # empty list means not implemented
             warnings.warn("Warning: action probability is not implemented for {} action space. Returning None."

common/distributions.py
@@ -299,24 +299,50 @@ class CategoricalProbabilityDistribution(ProbabilityDistribution):
     def action_mask_ph(self):
         return self._action_mask_ph
 
+    @property
+    def neginf_action_mask_ph(self):
+        """
+        negative inf action mask
+        Remap the values of the action mask, replacing 0s with -np.inf, and 1s with 0s.
+        """
+        negative_inf_vector = tf.ones_like(self._action_mask_ph, dtype=tf.float32) * -np.inf
+        zero_vector = tf.zeros_like(self._action_mask_ph, dtype=tf.float32)
+        _action_mask_ph = tf.where(tf.cast(self._action_mask_ph, dtype=tf.bool), zero_vector, negative_inf_vector)
+        return _action_mask_ph
+
     def flatparam(self):
         return self.logits
 
     def mode(self):
-        # return tf.argmax(self.logits, axis=-1)
         # mask: 0 is valid action, -inf is invalid action
-         # [1, 2, 3] add [0, -inf, 0] = [1, -inf, 3]
-        logits = self.logits
-        logits = tf.add(logits, self.action_mask_ph)
+        # [1, 2, 3] add [0, -inf, 0] = [1, -inf, 3]
+        logits = tf.add(self.logits, self.neginf_action_mask_ph)
         return tf.argmax(logits, axis=-1)
 
     def neglogp(self, x):
         # Note: we can't use sparse_softmax_cross_entropy_with_logits because
         #       the implementation does not allow second-order derivatives...
         one_hot_actions = tf.one_hot(x, self.logits.get_shape().as_list()[-1])
-        return tf.nn.softmax_cross_entropy_with_logits_v2(
-            logits=self.logits,
-            labels=tf.stop_gradient(one_hot_actions))
+        one_hot_actions = tf.stop_gradient(one_hot_actions)
+
+        # Prevent invalid actions backpropagation
+        self.logits = tf.multiply(self.logits, self.action_mask_ph)
+
+        # Calculate softmax and correct the invalid action probability to 0
+        softmax = tf.nn.softmax(self.logits)
+        exp_logits = softmax * tf.reduce_sum(tf.exp(self.logits), axis=-1, keepdims=True)
+        exp_logits = tf.multiply(exp_logits, self.action_mask_ph)
+        softmax = exp_logits / tf.reduce_sum(exp_logits, axis=-1, keepdims=True)
+
+        # softmax: [0.5, 0, 0.2, 0.3]
+        # action_mask: [1, 0, 1, 1]
+        # new_softmax: [0.5, 1, 0.2, 0.3]
+        softmax = tf.add(softmax, tf.cast(tf.logical_not(tf.cast(self.action_mask_ph, dtype=tf.bool)), dtype=tf.float32))
+
+        softmax_log = -tf.log(softmax)
+        softmax_cross_entropy = tf.reduce_sum(tf.multiply(softmax_log, one_hot_actions), axis=-1)
+
+        return softmax_cross_entropy
 
     def kl(self, other):
         a_0 = self.logits - tf.reduce_max(self.logits, axis=-1, keepdims=True)
@@ -329,8 +355,10 @@ class CategoricalProbabilityDistribution(ProbabilityDistribution):
         return tf.reduce_sum(p_0 * (a_0 - tf.log(z_0) - a_1 + tf.log(z_1)), axis=-1)
 
     def entropy(self):
-        a_0 = self.logits - tf.reduce_max(self.logits, axis=-1, keepdims=True)
+        logits = tf.multiply(self.logits, self.action_mask_ph)
+        a_0 = logits - tf.reduce_max(logits, axis=-1, keepdims=True)
         exp_a_0 = tf.exp(a_0)
+        exp_a_0 = tf.multiply(exp_a_0, self.action_mask_ph)
         z_0 = tf.reduce_sum(exp_a_0, axis=-1, keepdims=True)
         p_0 = exp_a_0 / z_0
         return tf.reduce_sum(p_0 * (tf.log(z_0) - a_0), axis=-1)
@@ -339,12 +367,11 @@ class CategoricalProbabilityDistribution(ProbabilityDistribution):
         # Gumbel-max trick to sample
         # a categorical distribution (see http://amid.fish/humble-gumbel)
         uniform = tf.random_uniform(tf.shape(self.logits), dtype=self.logits.dtype)
-        # return tf.argmax(self.logits - tf.log(-tf.log(uniform)), axis=-1)
         probability = self.logits - tf.log(-tf.log(uniform))
 
         # mask: 0 is valid action, -inf is invalid action
         # [1, 2, 3] add [0, -inf, 0] = [1, -inf, 3]
-        probability = tf.add(probability, self.action_mask_ph)
+        probability = tf.add(probability, self.neginf_action_mask_ph)
         return tf.argmax(probability, axis=-1)
 
     @classmethod

common/policies.py
@@ -532,7 +532,8 @@ class LstmPolicy(RecurrentActorCriticPolicy):
     def step(self, obs, state=None, mask=None, deterministic=False, action_mask=None):
         feed_dict = {self.obs_ph: obs, self.states_ph: state, self.dones_ph: mask}
         if action_mask is not None and len(action_mask) != 0:
-            feed_dict[self.action_mask_ph] = self.prepare_action_mask(action_mask)
+            # feed_dict[self.action_mask_ph] = self.prepare_action_mask(action_mask)
+            feed_dict[self.action_mask_ph] = np.array(action_mask, dtype=np.float32)
         if deterministic:
             return self.sess.run([self.deterministic_action, self.value_flat, self.snew, self.neglogp],
                                  feed_dict)
@@ -543,7 +544,8 @@ class LstmPolicy(RecurrentActorCriticPolicy):
     def proba_step(self, obs, state=None, mask=None, action_mask=None):
         feed_dict = {self.obs_ph: obs, self.states_ph: state, self.dones_ph: mask}
         if action_mask is not None and len(action_mask) != 0:
-            feed_dict[self.action_mask_ph] = self.prepare_action_mask(action_mask)
+            # feed_dict[self.action_mask_ph] = self.prepare_action_mask(action_mask)
+            feed_dict[self.action_mask_ph] = np.array(action_mask, dtype=np.float32)
         return self.sess.run(self.policy_proba, feed_dict)
 
     def value(self, obs, state=None, mask=None):
@@ -606,7 +608,9 @@ class FeedForwardPolicy(ActorCriticPolicy):
     def step(self, obs, state=None, mask=None, deterministic=False, action_mask=None):
         feed_dict = {self.obs_ph: obs}
         if action_mask is not None and len(action_mask) != 0:
-            feed_dict[self.action_mask_ph] = self.prepare_action_mask(action_mask)
+            # feed_dict[self.action_mask_ph] = self.prepare_action_mask(action_mask)
+            # feed_dict[self.action_mask_ph] = np.array(action_mask, dtype=np.float32)
+            feed_dict[self.action_mask_ph] = action_mask
         if deterministic:
             action, value, neglogp = self.sess.run([self.deterministic_action, self.value_flat, self.neglogp],
                                                    feed_dict)
@@ -618,7 +622,9 @@ class FeedForwardPolicy(ActorCriticPolicy):
     def proba_step(self, obs, state=None, mask=None, action_mask=None):
         feed_dict = {self.obs_ph: obs}
         if action_mask is not None and len(action_mask) != 0:
-            feed_dict[self.action_mask_ph] = self.prepare_action_mask(action_mask)
+            # feed_dict[self.action_mask_ph] = self.prepare_action_mask(action_mask)
+            # feed_dict[self.action_mask_ph] = np.array(action_mask, dtype=np.float32)
+            feed_dict[self.action_mask_ph] = action_mask
         return self.sess.run(self.policy_proba, feed_dict)
 
     def value(self, obs, state=None, mask=None):

ppo2/ppo2.py
@@ -242,7 +242,7 @@ class PPO2(ActorCriticRLModel):
 
                 self.summary = tf.summary.merge_all()
 
-    def _train_step(self, learning_rate, cliprange, obs, returns, masks, actions, values, neglogpacs, update,
+    def _train_step(self, learning_rate, cliprange, obs, returns, masks, actions, values, neglogpacs, action_masks, update,
                     writer, states=None, cliprange_vf=None):
         """
         Training of PPO2 Algorithm
@@ -266,6 +266,7 @@ class PPO2(ActorCriticRLModel):
         advs = (advs - advs.mean()) / (advs.std() + 1e-8)
         td_map = {self.train_model.obs_ph: obs, self.action_ph: actions,
                   self.advs_ph: advs, self.rewards_ph: returns,
+                  self.train_model.action_mask_ph: action_masks,
                   self.learning_rate_ph: learning_rate, self.clip_range_ph: cliprange,
                   self.old_neglog_pac_ph: neglogpacs, self.old_vpred_ph: values}
         if states is not None:
@@ -336,7 +337,7 @@ class PPO2(ActorCriticRLModel):
                 # true_reward is the reward without discount
                 rollout = self.runner.run(callback)
                 # Unpack
-                obs, returns, masks, actions, values, neglogpacs, states, ep_infos, true_reward = rollout
+                obs, returns, masks, actions, values, neglogpacs, states, ep_infos, true_reward, action_masks = rollout
 
                 callback.on_rollout_end()
 
@@ -356,7 +357,7 @@ class PPO2(ActorCriticRLModel):
                                                                             self.n_batch + start) // batch_size)
                             end = start + batch_size
                             mbinds = inds[start:end]
-                            slices = (arr[mbinds] for arr in (obs, returns, masks, actions, values, neglogpacs))
+                            slices = (arr[mbinds] for arr in (obs, returns, masks, actions, values, neglogpacs, action_masks))
                             mb_loss_vals.append(self._train_step(lr_now, cliprange_now, *slices, writer=writer,
                                                                  update=timestep, cliprange_vf=cliprange_vf_now))
                 else:  # recurrent version
@@ -373,7 +374,7 @@ class PPO2(ActorCriticRLModel):
                             end = start + envs_per_batch
                             mb_env_inds = env_indices[start:end]
                             mb_flat_inds = flat_indices[mb_env_inds].ravel()
-                            slices = (arr[mb_flat_inds] for arr in (obs, returns, masks, actions, values, neglogpacs))
+                            slices = (arr[mb_flat_inds] for arr in (obs, returns, masks, actions, values, neglogpacs, action_masks))
                             mb_states = states[mb_env_inds]
                             mb_loss_vals.append(self._train_step(lr_now, cliprange_now, *slices, update=timestep,
                                                                  writer=writer, states=mb_states,
@@ -467,9 +468,13 @@ class Runner(AbstractEnvRunner):
             - infos: (dict) the extra information of the model
         """
         # mb stands for minibatch
-        mb_obs, mb_rewards, mb_actions, mb_values, mb_dones, mb_neglogpacs = [], [], [], [], [], []
+        mb_obs, mb_rewards, mb_actions, mb_values, mb_dones, mb_neglogpacs, mb_action_masks = [], [], [], [], [], [], []
         mb_states = self.states
         ep_infos = []
+        self.action_masks.clear()
+        self.action_masks.append(flatten_action_mask(self.env.action_space, self.env.get_attr("valid_actions")))
+        assert len(self.action_masks) == 1
+        self.action_masks = self.action_masks[-1]
         for _ in range(self.n_steps):
             actions, values, self.states, neglogpacs = self.model.step(self.obs, self.states, self.dones, action_mask=self.action_masks)
             mb_obs.append(self.obs.copy())
@@ -477,12 +482,12 @@ class Runner(AbstractEnvRunner):
             mb_values.append(values)
             mb_neglogpacs.append(neglogpacs)
             mb_dones.append(self.dones)
+            mb_action_masks.append(self.action_masks.copy())
             clipped_actions = actions
             # Clip the actions to avoid out of bound error
             if isinstance(self.env.action_space, gym.spaces.Box):
                 clipped_actions = np.clip(actions, self.env.action_space.low, self.env.action_space.high)
             self.obs[:], rewards, self.dones, infos = self.env.step(clipped_actions)
-            self.action_masks.clear()
             self.model.num_timesteps += self.n_envs
 
             if self.callback is not None:
@@ -490,20 +495,24 @@ class Runner(AbstractEnvRunner):
                 if self.callback.on_step() is False:
                     self.continue_training = False
                     # Return dummy values
-                    return [None] * 9
+                    return [None] * 10
 
+            self.action_masks.clear()
             for info in infos:
                 maybe_ep_info = info.get('episode')
                 if maybe_ep_info is not None:
                     ep_infos.append(maybe_ep_info)
                 # action mask
 
-                if 'reset_action_mask' in info:
-                    action_mask = self.env.get_attr("valid_actions")[0]
-                    self.action_masks.append(flatten_action_mask(self.env.action_space, action_mask))
-                else:
-                    env_action_mask = info.get('action_mask')
-                    self.action_masks.append(flatten_action_mask(self.env.action_space, env_action_mask))
+                # if 'reset_action_mask' in info:
+                #     action_mask = self.env.get_attr("valid_actions")[0]
+                #     self.action_masks.append(flatten_action_mask(self.env.action_space, action_mask))
+                # else:
+                #     env_action_mask = info.get('action_mask')
+                #     self.action_masks.append(flatten_action_mask(self.env.action_space, env_action_mask))
+            self.action_masks.append(flatten_action_mask(self.env.action_space, self.env.get_attr("valid_actions")))
+            assert len(self.action_masks) == 1
+            self.action_masks = self.action_masks[-1]
             mb_rewards.append(rewards)
         # batch of steps to batch of rollouts
         mb_obs = np.asarray(mb_obs, dtype=self.obs.dtype)
@@ -512,6 +521,7 @@ class Runner(AbstractEnvRunner):
         mb_values = np.asarray(mb_values, dtype=np.float32)
         mb_neglogpacs = np.asarray(mb_neglogpacs, dtype=np.float32)
         mb_dones = np.asarray(mb_dones, dtype=np.bool)
+        mb_action_masks = np.asfarray(mb_action_masks, dtype=np.float32)
         last_values = self.model.value(self.obs, self.states, self.dones)
         # discount/bootstrap off value fn
         mb_advs = np.zeros_like(mb_rewards)
@@ -528,10 +538,10 @@ class Runner(AbstractEnvRunner):
             mb_advs[step] = last_gae_lam = delta + self.gamma * self.lam * nextnonterminal * last_gae_lam
         mb_returns = mb_advs + mb_values
 
-        mb_obs, mb_returns, mb_dones, mb_actions, mb_values, mb_neglogpacs, true_reward = \
-            map(swap_and_flatten, (mb_obs, mb_returns, mb_dones, mb_actions, mb_values, mb_neglogpacs, true_reward))
+        mb_obs, mb_returns, mb_dones, mb_actions, mb_values, mb_neglogpacs, true_reward, mb_action_masks = \
+            map(swap_and_flatten, (mb_obs, mb_returns, mb_dones, mb_actions, mb_values, mb_neglogpacs, true_reward, mb_action_masks))
 
-        return mb_obs, mb_returns, mb_dones, mb_actions, mb_values, mb_neglogpacs, mb_states, ep_infos, true_reward
+        return mb_obs, mb_returns, mb_dones, mb_actions, mb_values, mb_neglogpacs, mb_states, ep_infos, true_reward, mb_action_masks
 
 
 # obs, returns, masks, actions, values, neglogpacs, states = runner.run()